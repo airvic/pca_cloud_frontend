6cea9822ed15935e623b1e5292e7f8d0391b0aac
airvic <airewamhevictor@gmail.com>
2022-11-02 07:50:50 -0700

    my first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+   apiKey: "AIzaSyBBoBswfAWQXTxjb1CpCxMgJOG7PqYkQwk",
+   authDomain: "pcacloud-dcda8.firebaseapp.com",
+   projectId: "pcacloud-dcda8",
+   storageBucket: "pcacloud-dcda8.appspot.com",
+   messagingSenderId: "290822244794",
+   appId: "1:290822244794:web:e05467efc24fca82347583",
+   measurementId: "G-861XP7FTY2"  },
+   AWS_ACCESS_KEY_ID: "AKIAW7RQIDGW6WRY5NBD",
+   AWS_SECRET_ACCESS_KEY: "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",
+   AWS_REGION: "us-east-1"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
    firebaseConfig : {
     apiKey: "AIzaSyBBoBswfAWQXTxjb1CpCxMgJOG7PqYkQwk",
     authDomain: "pcacloud-dcda8.firebaseapp.com",
