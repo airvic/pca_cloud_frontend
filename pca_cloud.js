22168aabc56613fcbe0f383c4ab20da38bfd2934
airvic <airewamhevictor@gmail.com>
2022-11-02 16:30:54 -0700

    first commit

--- pca_cloud.js
+++ pca_cloud.js
@@ -41,6 +41,7 @@ var diskstorage = Multer.diskStorage({
 
 
   app.use(bodyparser.json())
+  app.use(express.static(path.join(__dirname,'public')));
 app.use('/images',express.static(path.join('images')))
   app.use(bodyparser.urlencoded({extended : true}))
 
@@ -63,6 +64,10 @@ app.use(function(req, res, next) {
     console.log('mongodb error : '+ error)
     
     
+  })
+  app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/index.html'))
+      
   })
 
 app.get('/home',(req,res)=>{
