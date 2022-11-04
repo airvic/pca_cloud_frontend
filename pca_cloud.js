015108afbada93f17a115f432edda4f83433aa82
airvic <airewamhevictor@gmail.com>
2022-11-04 02:25:55 -0700

    my first commit

--- pca_cloud.js
+++ pca_cloud.js
@@ -67,11 +67,11 @@ app.use(function(req, res, next) {
     
   })
   app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public/index.html'))
+    res.sendFile(path.join(__dirname,'public/index.html/dasboard'))
       
   })
   app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname+'public/index.html'))
+    res.sendFile(path.join(__dirname+'/public/index.html'));
   })
 app.get('/home',(req,res)=>{
     res.send(true)
